4e9b8f94eb00a7350a33c74390d606c095146042
Nonso Mgbechi <mgbechinonso@gmail.com>
2018-09-26 15:45:53 +0100

    commit main Chat class

---
+++ core/classes/Chat.php
@@ -0,0 +1,24 @@
+<?php
+class Chat extends Core {
+    public function fetchMessages() {
+        // query db
+        // return rows
+        $sql = "SELECT `chats`.`message`, `users`.`name`, `users`.`id`
+        FROM `chats` INNER JOIN `users` ON `chats`.`user_id`=`users`.`id` ORDER BY `chats`.`timestamp` DESC";  
+        return $this->query($sql);       
+    }
+
+    public function throwMessage($user_id, $message) {
+        // Insert db
+        $sql = "INSERT INTO `chats` (`user_id`, `message`, `timestamp`)
+            VALUES (".(int)$user_id.", '". $this->checkInput($message)."', UNIX_TIMESTAMP())";
+        $this->db->exec($sql);
+    }
+
+    public function checkInput($value) {
+        $value = trim($value);
+        $value = stripslashes($value);
+        $value = htmlspecialchars($value);
+        return $value;
+    }
+}
