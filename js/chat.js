4ac5b9468521aaad1e728b3e519b23d1c85db840
Nonso Mgbechi <mgbechinonso@gmail.com>
2018-10-01 22:28:04 +0100

    commit throwMessage js

--- js/chat.js
+++ js/chat.js
@@ -12,7 +12,37 @@ chat.fetchMessages = function() {
             $(".chat .messages").html(data)
         }
     })
+} 
+
+chat.throwMessage = function(message) {
+    // Save this message if it's not empty
+    if ($.trim(message) != "") {
+        $.ajax({
+            url: "ajax.chat.php",
+            type: "post",
+            data: {
+                method: "throw",
+                message: message
+            },
+            success: function(data) {
+                chat.fetchMessages()
+            }
+        })
+    }
 }
+
+// variable for chat entry area
+chat.entry = $(".chat .entry")
+
+// Bind eventhandler to 'enter' key in chat.entry
+chat.entry.bind('keydown', function(e) {
+    // Post message if enter key is pressed
+    if (e.keyCode === 13 && !e.shiftKey) {
+        chat.throwMessage(e.target.val())
+        $(this).val("")
+        e.preventDefault() // basically 'enter' shouldn't move the cursor to next line
+    }
+})
 
 // To set interval for chat refresh
 chat.interval = setInterval(chat.fetchMessages, 5000)
